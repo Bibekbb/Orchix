ec8585876263b272f66c34ff734a616269d0da11
Bibek Bhattarai <bb.bibekbhattarai@gmail.com>
2025-12-21 00:15:03 +0545

    add kubernetes

--- internal/providers/kubernetes/provider.go
+++ internal/providers/kubernetes/provider.go
@@ -5,5 +5,55 @@ import (
 	"fmt"
 	"time"
 
-	appsv1 "k8s.io/api/"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"orchestr8/pkg/types"
 )
+
+types KubernetesProvider struct {
+	client   	kubernetes.interface
+	dynamic 	dynamic.interface
+	config 		*rest.Config
+}
+
+func NewKubernetesProvider(kubeconfigPath string) (*KubernetesProvider, error){
+	var config *rest.Config
+	var err error 
+
+	if kubeconfigPath != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	} else {
+		config, err = rest.InClusterConfig()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KubernetesProvider{
+		client: client,
+		dynamic: dynamicClient,
+		config: config,
+	}, nil 
+}
